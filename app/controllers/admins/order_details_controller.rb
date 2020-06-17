00a847614ec7985ca02145dbdf16c7901b5c5c7c
vagrant <vagrant@localhost.localdomain>
2020-06-17 10:26:49 +0000

    アクション記述

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -1,2 +1,6 @@
 class Admins::OrderDetailsController < ApplicationController
+
+	def update
+	end
+	
 end
--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -1,2 +1,21 @@
 class Customers::OrdersController < ApplicationController
+
+	def about
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def complete
+	end
+
+	def index
+	end
+
+	def show
+	end
+
 end
