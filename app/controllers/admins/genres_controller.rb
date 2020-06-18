3636fefc0dbe65af502536e8151b9364da440510
kasyu-create <vagrant@localhost.localdomain>
2020-06-18 08:02:16 +0000

    genres/index実装大体完了 genres_controllerのcreateアクションとgenre_params追加

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -5,11 +5,23 @@ class Admins::GenresController < ApplicationController
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admins_genres_path
+    else
+      flash[:genre_created_error] = "ジャンル名を入力してください"
+    redirect_to
+  end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name, :is_genres_status)
   end
 end
