1fc3408884bf2858a2a66ffe3897e2ddf3d2a105
vagrant <vagrant@localhost.localdomain>
2020-06-18 14:17:30 +0000

    itemsコントローラにindex,showアクションを作成

--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -1,5 +1,14 @@
 class Customers::ItemsController < ApplicationController
+
  def top
-    
  end
+
+ def index
+ 	@items = Item.all
+ end
+
+ def show
+ 	@item = Item.find(params[:id])
+ end
+
 end
