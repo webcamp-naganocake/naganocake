7b9de51823be1689347a0dfd06fe8f55755144e3
vagrant <vagrant@localhost.localdomain>
2020-06-18 08:20:04 +0000

    cart_itemsのviewsを一部作成

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -1,5 +1,6 @@
 class Customers::CartItemsController < ApplicationController
-    
+    before_action :authenticate_customer!
+
     def index
         @cart_items = CartItem.all
     end
@@ -9,20 +10,28 @@ class Customers::CartItemsController < ApplicationController
         if @cart_item.save
            flash[:notice] = "#{@cart_item.item.name}をカートに追加しました。"
            redirect_to customers_cart_items_path
-        else    
+        else
+            flash[:alert] = "個数を選択してください"
+            render "customers/items/show"
         end
     end
 
     def update
-
+        再計算ボタン
     end
 
     def destroy
-
+        @cart_item = CartItem.find(params[:item_id])
+        @cart_item.destroy
+        flash.now[:alert] = "#{@cart_item.item.name}を削除しました"
+        合計金額を再計算するアクション
     end
 
     def all_destroy
-
+        @cart_item = current_customer.cart_items
+        @cart_item.destroy
+        flash[:alert] = "カートの商品を全て削除しました"
+        redirect_to customers_cart_items_path
     end
 
     private
