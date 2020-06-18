a5ff5e91d9bd1df87e4ce8885b9cb614ed8e334c
vagrant <vagrant@localhost.localdomain>
2020-06-18 12:33:52 +0000

    _headerのログアウトpathを追加

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 2020_06_18_064032) do
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postal_code"
+    t.string "address"
+    t.string "phone_number"
+    t.boolean "is_customer_status", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
