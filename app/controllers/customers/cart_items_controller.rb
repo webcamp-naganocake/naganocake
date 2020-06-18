32b295a699e43daf5d6fcdfa2782716848e03c46
vagrant <vagrant@localhost.localdomain>
2020-06-18 10:56:59 +0000

    コントローラーを少し修正

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -6,7 +6,7 @@ class Customers::CartItemsController < ApplicationController
     end
 
     def create
-        @cart_item = current_customer.cart_items.build(item_id: params[:item_id])
+        @cart_item = current_customer.cart_items.build(cart_item_params)
         if @cart_item.save
            flash[:notice] = "#{@cart_item.item.name}をカートに追加しました。"
            redirect_to customers_cart_items_path
@@ -17,11 +17,18 @@ class Customers::CartItemsController < ApplicationController
     end
 
     def update
-        再計算ボタン
+        @cart_item = CartItem.find(params[:id])
+        if @cart_item.update(cart_item_params)
+           flash[:notice] = "#{@cart_item.item.name}をカートに追加しました。"
+           redirect_to customers_cart_items_path
+        else
+            flash[:alert] = "個数を選択してください"
+            render "customers/items/show"
+        end
     end
 
     def destroy
-        @cart_item = CartItem.find(params[:item_id])
+        @cart_item = CartItem.find(params[:id])
         @cart_item.destroy
         flash.now[:alert] = "#{@cart_item.item.name}を削除しました"
         合計金額を再計算するアクション
@@ -38,4 +45,5 @@ class Customers::CartItemsController < ApplicationController
       def cart_item_params
         params.require(:cart_item).permit(:quantity, :item_id, :customer_id)
       end
+
 end
