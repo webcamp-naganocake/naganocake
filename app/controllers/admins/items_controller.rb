11447ab02a34ba2f36e9071cb5b92edc75c95940
kasyu-create <vagrant@localhost.localdomain>
2020-06-19 08:41:17 +0000

    admin/items/indexページ実装 admin_items_controllerのnewアクション追記 itemモデル追記

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,5 +1,6 @@
 class Admins::ItemsController < ApplicationController
   def show
+    @item = Item.find(params[:id])
   end
 
   def index
@@ -11,11 +12,23 @@ class Admins::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admins_items_path
+    else
+      flash[:genre_created_error] = "ジャンル名を入力してください"
+    redirect_to
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :description, :price_without_tax, :image_id, :is_sales_status)
   end
 end
