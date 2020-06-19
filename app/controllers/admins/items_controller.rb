a43cd317a8b270a3d1f54b1e215bd4c7cc75519c
kasyu-create <vagrant@localhost.localdomain>
2020-06-19 14:17:34 +0000

    admin/items/editページ実装 admin_items_controllerのedit updateアクション追記

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -17,14 +17,22 @@ class Admins::ItemsController < ApplicationController
       redirect_to admins_items_path
     else
       flash[:genre_created_error] = "ジャンル名を入力してください"
-    redirect_to
+    redirect_to new_admins_item_path
     end
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admins_items_path(@item)
+      flash[:notice_update] = "ジャンル情報を更新しました！"
+    else
+      redirect_to edit_admins_item_path(@item)
+    end
   end
 
   private
