c8fd5054f2070dfa8acb59f5fc66a419422a7282
vagrant <vagrant@localhost.localdomain>
2020-06-19 08:25:10 +0000

    ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	#ログイン後に遷移するpathを設定
+	def after_sign_in_path_for(resource)
+		customer_path(resource)
+	end
+
+	protected　
+	#sign_out後のredirect先
+	def after_sign_out_path_for(resource)
+		customer_path(resource)
+	end
+	def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+	end
 end
