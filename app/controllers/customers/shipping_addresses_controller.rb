decdd1321fa5165c12bcdde9e6258593e0043843
vagrant <vagrant@localhost.localdomain>
2020-06-20 03:57:16 +0000

    編集ページを追記

--- app/controllers/customers/shipping_addresses_controller.rb
+++ app/controllers/customers/shipping_addresses_controller.rb
@@ -1,2 +1,28 @@
 class Customers::ShippingAddressesController < ApplicationController
+    
+    def index
+        @shipping_addresses = ShippingAddress.all
+    end
+    
+    def create
+        
+    end
+
+    def destory
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+  private
+    def shipping_address_params
+        params.require(:shipping_address).permit(:customer_id, :name, :postal_code, :address)
+    end
+
 end
