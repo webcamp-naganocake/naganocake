b9db267763f22c229e3dfecb462767f1a8d7cdc2
kasyu-create <vagrant@localhost.localdomain>
2020-06-20 07:20:34 +0000

    admin/customers/itページ実装 admin_customers_controllerのshedit,updateクション追記 kaminari導入 モデルcustomerにvalidates追加

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,6 +1,6 @@
 class Admins::CustomersController < ApplicationController
   def index
-    @customers = Customer.all
+    @customers = Customer.page(params[:page]).per(10)
   end
 
   def show
@@ -8,13 +8,21 @@ class Admins::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admins_customer_path(@customer)
+      flash[:notice_update] = "ジャンル情報を更新しました！"
+    else
+      edit_admins_customer_path(@customer)
+    end
   end
 
   private
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email, :is_customer_status)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email, :is_customer_status)
   end
 end
