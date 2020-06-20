405df44b411c64cee7dcc6591e0e91a13ad945b3
vagrant <vagrant@localhost.localdomain>
2020-06-20 12:50:26 +0000

    編集中

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -2,7 +2,16 @@
 
 class Customers::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+ 
+  #ログイン後に遷移するpathを設定
+  def after_sign_in_path_for(resource)
+    root_path(resource)
+  end
+  #sign_out後のredirect先
+  def after_sign_out_path_for(resource)
+    new_customer_session_path
+  end
   # GET /resource/sign_in
   # def new
   #   super
@@ -18,8 +27,10 @@ class Customers::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
-
+  protected
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number,])
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
