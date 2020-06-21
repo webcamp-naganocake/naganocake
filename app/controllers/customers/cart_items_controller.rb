3a8f683429c5abca583132aa280354796d20fe89
vagrant <vagrant@localhost.localdomain>
2020-06-21 09:12:28 +0000

    8割完成

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -1,12 +1,19 @@
 class Customers::CartItemsController < ApplicationController
     before_action :authenticate_customer!
 
+    # カート商品一覧を表示
     def index
         @cart_items = current_customer.cart_items
     end
 
+    # カート商品を追加する
     def create
-        @cart_item = current_customer.cart_items.build(cart_item_params)
+        # @cart_item = current_customer.cart_items.build(item_id: params[:item_id])
+        @cart_item = CartItem.new(cart_item_params)
+        @cart_item.customer_id = current_customer.id
+        @cart_item.item_id = params[:item_id]
+        # byebug
+
         if @cart_item.save
            flash[:notice] = "#{@cart_item.item.name}をカートに追加しました。"
            redirect_to customers_cart_items_path
@@ -16,32 +23,32 @@ class Customers::CartItemsController < ApplicationController
         end
     end
 
+    # 削除や個数を変更した際、カート商品を再計算する
     def update
         @cart_item = CartItem.find(params[:id])
-        if @cart_item.update(cart_item_params)
-           flash[:notice] = "#{@cart_item.item.name}をカートに追加しました。"
-           redirect_to customers_cart_items_path
-        else
-            flash[:alert] = "個数を選択してください"
-            render "customers/items/show"
-        end
+        #@cart.units += cart_params[:units].to_i
+        @cart_item.update(cart_item_params)
+        redirect_to customers_cart_items_path
     end
 
+    # カート商品を一つのみ削除
     def destroy
         @cart_item = CartItem.find(params[:id])
         @cart_item.destroy
         flash.now[:alert] = "#{@cart_item.item.name}を削除しました"
-        合計金額を再計算するアクション
+        redirect_to customers_cart_items_path
     end
 
+    # カート商品を空ににする
     def all_destroy
         @cart_item = current_customer.cart_items
-        @cart_item.destroy
+        @cart_item.destroy_all
         flash[:alert] = "カートの商品を全て削除しました"
         redirect_to customers_cart_items_path
     end
 
     private
+
       def cart_item_params
         params.require(:cart_item).permit(:quantity, :item_id, :customer_id)
       end
