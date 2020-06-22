1f599be663a1a040b860c62f1ce65e68d01b4112
kasyu-create <vagrant@localhost.localdomain>
2020-06-22 01:05:02 +0000

    develop移動の為

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,10 +1,11 @@
 class Admins::OrdersController < ApplicationController
 
 	def index
-		@orders = Order.all
+		@orders = Order.page(params[:page]).per(10)
 	end
 
 	def show
+		@order = Order.find(params[:id])
 	end
 
 	def update
