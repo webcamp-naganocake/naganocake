c4c405b1aa061a9cc26864c0df5200aaf524d52b
vagrant <vagrant@localhost.localdomain>
2020-06-23 09:15:52 +0000

    エラーメッセージ編集

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Naganocake
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    #日本語化を反映させる
+	config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_124903) do
+ActiveRecord::Schema.define(version: 2020_06_18_064032) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,13 +35,13 @@ ActiveRecord::Schema.define(version: 2020_06_21_124903) do
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.string "postal_code"
-    t.string "address"
-    t.string "phone_number"
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.string "postal_code", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "phone_number", default: "", null: false
     t.boolean "is_customer_status", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
