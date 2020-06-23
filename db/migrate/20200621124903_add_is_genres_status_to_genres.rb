9d311717b762a05e63a0e8014608b855c7204356
vagrant <vagrant@localhost.localdomain>
2020-06-23 06:46:33 +0000

    a

--- db/migrate/20200621124903_add_is_genres_status_to_genres.rb
+++
@@ -1,5 +0,0 @@
-class AddIsGenresStatusToGenres < ActiveRecord::Migration[5.2]
-  def change
-    add_column :genres, :is_genres_status, :boolean, default: false, null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_124903) do
+ActiveRecord::Schema.define(version: 2020_06_18_064032) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 2020_06_21_124903) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.boolean "is_genres_status", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_genres_status", default: false, null: false
