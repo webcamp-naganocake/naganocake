0b256c08a10b88e474bd7311c9b2c4ee186c0e0b
vagrant <vagrant@localhost.localdomain>
2020-06-23 08:34:01 +0000

    ordersコントローラ編集

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -2,17 +2,35 @@ class Customers::OrdersController < ApplicationController
 
 	def about
 		@order = Order.new(order_params)
+		@customer = current_customer
+		@addresses = ShippingAddress.where(customer_id: current_customer.id)
 	end
 
-	def new
+	def save
 		@order = Order.new(order_params)
-		@order.customer_id = current_customer.id
-		# 確認画面へのパス確定後、コメントアウト解除
-		# if @order.save
-			# redirect_to 確認画面へのパス
-		# else
-			# render 'customers_orders_about'
-		# end
+		@order.customer_id = current_customer
+		@a = params[:order][:a_mathod]
+		if @a == 0
+			@order.post_code = @customer.postal_code
+			@order.address = @customer.address
+			@order.name = @customer.last_name + @customer_first_name
+			@order.save
+			redirect_to customers_orders_path
+		elsif @a == 1
+			@order.post_code = params[:order][:shipping_address_for_order].postal_code
+			@order.address = params[:order][:shipping_address_for_order].address
+			@order.name = params[:order][:shipping_address_for_order].name 
+			@order.save
+			redirect_to customers_orders_path
+		elsif @a == 2
+			@order.save
+			redirect_to customers_orders_path
+		else
+			render 'customers_orders_about'
+		end
+	end
+
+	def confirm
 	end
 
 	def create
@@ -32,7 +50,6 @@ class Customers::OrdersController < ApplicationController
 
 	private
     def order_params
-       params.require(:order).permit(:customer_id, :postage, :total_payment, :payment_method, :ordr_status, :post_code, :address, :name)
+       params.permit(:postage, :total_payment, :payment_method, :ordr_status, :post_code, :address, :name)
     end
 end
-
