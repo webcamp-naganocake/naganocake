1a0587663a8727674911333f095b4465c98f8541
vagrant <vagrant@localhost.localdomain>
2020-06-26 00:12:10 +0000

    enum被り消去

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,8 +4,6 @@ class Order < ApplicationRecord
 
   enum payment_method: { クレジットカード: 0, 銀行振込: 1 }
   enum order_status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4 }
-  
-  enum payment_method: { "クレジットカード": 0, "銀行振込": 1 }
-  enum order_status: { "入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4 }
+
 
 end
