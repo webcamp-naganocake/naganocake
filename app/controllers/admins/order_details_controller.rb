a0bba2f8004c2aa079a727a920196b35a46ef568
kasyu-create <vagrant@localhost.localdomain>
2020-06-26 07:10:09 +0000

    admin側注文完成

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -2,19 +2,18 @@ class Admins::OrderDetailsController < ApplicationController
 
 	def update
 		order_detail =
-		OrderDetail.find(id: order_detail_id)
+		OrderDetail.find(params[:id])
 		order_detail.update(order_detail_params)
 
 		case order_detail.making_status
 		 when "製作中"
 				order_detail.order.update(order_status: "製作中")
 		 when "製作完了"
-			if
-				order_detail.order.order_details.all?{|order_details| order_detail.making_status == "製作完了"}
+			if order_detail.order.order_details.all?{|order_detail| order_detail.making_status == "製作完了"}
 				order_detail.order.update(order_status: "発送準備中")
 			end
 		end
-		redirect_to admins_order_path(order_detail)
+		redirect_to admins_order_path(order_detail.order.id)
 	end
 
 	private
