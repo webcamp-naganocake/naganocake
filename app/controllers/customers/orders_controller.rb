f575c21251be5566046e00b9a49addf40892b864
vagrant <vagrant@localhost.localdomain>
2020-06-27 00:31:54 +0000

    orderコントローラコメントアウト＆サンクスページ編集

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -8,43 +8,52 @@ class Customers::OrdersController < ApplicationController
 
 	def create
 		customer = current_customer
+
+		# sessionを使ってデータを一時保存
 		session[:order] = Order.new
 
-
 		cart_items = current_customer.cart_items
+
+		# total_paymentのための計算
 		sum = 0
 		cart_items.each do |cart_item|
 			sum += (cart_item.item.price_without_tax * 1.1).floor * cart_item.quantity
 		end
+
 		session[:order][:postage] = 800
 		session[:order][:total_payment] = sum + session[:order][:postage]
 		session[:order][:order_status] = 0
 		session[:order][:customer_id] = current_customer.id
+		# ラジオボタンで選択された支払方法のenum番号を渡している
 		session[:order][:payment_method] = params[:method].to_i
 
-		a = params[:a_method].to_i
-		if a == 0
+		# ラジオボタンで選択されたお届け先によって条件分岐
+		destination = params[:a_method].to_i
+
+		# ご自身の住所が選択された時
+		if destination == 0
 
 			session[:order][:post_code] = customer.postal_code
 			session[:order][:address] = customer.address
 			session[:order][:name] = customer.last_name + customer.first_name
 
-		elsif a == 1
+		# 登録済住所が選択された時
+		elsif destination == 1
 
 			address = ShippingAddress.find(params[:shipping_address_for_order])
 			session[:order][:post_code] = address.postal_code
 			session[:order][:address] = address.address
 			session[:order][:name] = address.name 
 
-
-
-		elsif a == 2
+		# 新しいお届け先が選択された時
+		elsif destination == 2
 			session[:order][:post_code] = params[:post_code]
 			session[:order][:address] = params[:address]
 			session[:order][:name] = params[:name]
 
 		end
 
+		# お届け先情報に漏れがあればリダイレクト
 		if session[:order][:post_code].presence && session[:order][:address].presence && session[:order][:name].presence
 			redirect_to new_customers_order_path
 		else
@@ -63,6 +72,7 @@ class Customers::OrdersController < ApplicationController
 	def complete
 		order = Order.new(session[:order])
 		order.save
+		# 以下、order_detail作成
 		cart_items = current_customer.cart_items
 		cart_items.each do |cart_item|
 			order_detail = OrderDetail.new
@@ -73,6 +83,7 @@ class Customers::OrdersController < ApplicationController
 			order_detail.price = (cart_item.item.price_without_tax * 1.1).floor
 			order_detail.save
 		end
+		# 購入後はカート内商品削除
 		cart_items.destroy_all
 	end
 
@@ -85,16 +96,16 @@ class Customers::OrdersController < ApplicationController
 		@order_details = @order.order_details
 	end
 
-	private
-	    def order_params
-	       params.require(:order).permit(:customer_id, :postage, :total_payment, :payment_method, :ordr_status, :post_code, :address, :name)
-	    end
+	# private
+	#     def order_params
+	#        params.require(:order).permit(:customer_id, :postage, :total_payment, :payment_method, :ordr_status, :post_code, :address, :name)
+	#     end
 
 
-	    def order_detail_params
-	       params.require(:order_detail).permit(:order_id, :item_id, :quantity, :making_status, :price)
-	    end
+	#     def order_detail_params
+	#        params.require(:order_detail).permit(:order_id, :item_id, :quantity, :making_status, :price)
+	#     end
 
-	end
+end
 
 
