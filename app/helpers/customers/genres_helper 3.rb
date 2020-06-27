d096cd838fcad4b5223bc2bed06f70653161bd9b
vagrant <vagrant@localhost.localdomain>
2020-06-27 03:57:25 +0000

    重複ファイル削除

--- app/helpers/customers/genres_helper 3.rb
+++
@@ -1,2 +0,0 @@
-module Customers::GenresHelper
-end
--- test/controllers/customers/genres_controller_test 2.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Customers::GenresControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get customers_genres_show_url
-    assert_response :success
-  end
-
-end
